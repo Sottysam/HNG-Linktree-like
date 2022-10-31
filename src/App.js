99f8f535f9c0a4e0b5079ab990e001ab8e76554b
sotundesammy <sotundesammy@gmail.com>
2022-10-31 10:51:01 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { links } from './data'
+import Pic1 from "./image/PhotoGrid_Plus_1648803761282.jpg"
+import Pic2 from "./image/I4G.png"
+import Pic3 from "./image/logo.36d2d48a.png"
+import Pic4 from "./image/Footer text.png"
+import social1 from "./image/slack.png"
+import social2 from "./image/Social icon.png"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <img src={Pic1} alt="profile_pic" className='profilePics' />
+      <h1>Sotunde Samuel</h1>
+      {links.map((link)=>{
+        const {id,name,url} = link
+        return(
+          <div key={id} className="hngLinks">
+            <a href={url} target="_blank"><h4>{name}</h4></a>
+          </div>
+        )
+      })}
+      <div className='icon'>
+        <img src={social1} alt="Slack" />
+        <img src={social2} alt="Github" />
+      </div>
+      <div className='footer'>
+        <img src={Pic3} alt="Zuri" />
+        <img src={Pic4} alt="Text" className='text' />
+        <img src={Pic2} alt="I4G" />
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const links=[
+  {
+    id: 1,
+    name: "twitter link",
+    url: "https://twitter.com/Sot_O_Sam",
+  },
+  {
+    id: 2,
+    name: "zuri team",
+    url: "http://books.zuri.team",
+  },
+  {
+    id: 3,
+    name: "zuri books",
+    url: "https://books.zuri.team",
+  },
+  {
+    id: 4,
+    name: "python books",
+    url: "https://books.zuri.team",
+  },
+  {
+    id: 5,
+    name: "background check for coders",
+    url: "https://background.zuri.team",
+  },
+  {
+    id: 6,
+    name: "design books",
+    url: "https://books.zuri.team/design-rules",
+  }
+]
